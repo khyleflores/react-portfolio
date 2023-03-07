0822f5262122f1e66d1d47e6b662cb1a0aa791a2
khyleflores <khyleflores10@gmail.com>
2023-03-07 17:17:16 +0000

    Added Home Page with links to socials

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,7 +1,21 @@
 import React from "react";
 import "../styles/global.css";
+import "../styles/about.css";
+import aboutPhoto from "../styles/images/my-photo-1.png";
 
 function About() {
+    return (
+        <div className ="container">
+          <div className="row" >
+            <div className="col-12 text-center sectionTitle">
+                <h1>About <span className="whiteFont">Me</span></h1>
+            </div>
+            <div className="col-lg-6 imageContainer">
+                <img className="img-fluid rounded" src={aboutPhoto} alt="Khyle Flores' photo/woman wearing white blazer, black top and white trouser"></img>
+            </div>
+          </div>
+        </div>
+      );
 }
 
 export default About;
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,6 +1,9 @@
 import React from "react";
 import "../styles/global.css";
+import "../styles/home.css";
 import homeLogo from "../styles/freepik-pikisuperstar.png";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faLinkedin, faTwitterSquare, faGithubSquare} from '@fortawesome/free-brands-svg-icons';
 
 
 function Home() {
@@ -8,10 +11,16 @@ function Home() {
     <div className ="container">
       <div className="row" >
         <div className="col-lg-6 col-md-6 col-sm-6 sectionHeading">
-            <h1><span className="h1Sub3heading">Hello, My Name is</span>
-            <br/>Khyle Flores
-            <br/><span className="h1Subheading">Front-End Developer</span>
-            <br/><span className="h1Sub2heading">#Bootstrap #Node #React #PHP</span></h1>
+            <h1>
+            <span className="h1Subheading">Hello, My Name is</span>
+            <br/><span className="h1Sub1heading">Khyle Flores</span>
+            <br/><span className="h1Sub2heading">Front-End Developer</span>
+            <br/><span className="h1Sub3heading">#Bootstrap #Node #React #PHP</span>
+            <br/><span className="h1SubIcons"><a target="_blank" href="https://www.linkedin.com/in/khyle-flores-2478a7b5/"><FontAwesomeIcon icon={faLinkedin} /></a>
+            <a target="_blank" href="https://twitter.com/KhyleFlores10"><FontAwesomeIcon icon={faTwitterSquare} /></a>
+            <a target="_blank" href="https://github.com/khyleflores"><FontAwesomeIcon icon={faGithubSquare} /></a>
+            </span>
+            </h1>
         </div>
         <div className="col-lg-6 col-md-6 col-sm-6 imageContainer">
             <img className="img-fluid homePhoto" src={homeLogo} alt="Designed by pikisuperstar / Freepik"/>
